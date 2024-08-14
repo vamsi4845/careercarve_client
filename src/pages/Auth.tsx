09e33d0c4b5364b354ed40b98c21399eaf599a3d
vamsisai <vamsisai.karumuri@gmail.com>
2024-08-15 01:20:48 +0530

    chore:update auth bg

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/clerk-react";
 
 export default function LoginPage() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-primary">
+    <div className="flex flex-row items-center justify-center min-h-screen bg-[url('/auth.png')] bg-cover bg-no-repeat">
       <SignIn />
     </div>
   );
