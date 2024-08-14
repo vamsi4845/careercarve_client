bba9dc71dadf0215e12b6c7d34ddc874c9676cbc
vamsisai <vamsisai.karumuri@gmail.com>
2024-08-15 00:59:29 +0530

    refractor:activity page

--- src/pages/Activity.tsx
+++ src/pages/Activity.tsx
@@ -3,27 +3,17 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 export default function Activity() {
   return (
     <div className='px-4 sm:px-10 mt-10'>
-        <h2 className='font-bold text-2xl'>Activity</h2>
+        <h2 className='font-bold text-2xl'>Appointment History</h2>
         <Tabs defaultValue="upcoming" className="w-full mt-5">
             <TabsList className="w-full">
-                <TabsTrigger value="upcoming">Upcoming</TabsTrigger>
-                <TabsTrigger value="expired">Expired</TabsTrigger>
+                <TabsTrigger value="upcoming">Upcoming Appointments</TabsTrigger>
+                <TabsTrigger value="expired">Past Appointments</TabsTrigger>
             </TabsList>
             <TabsContent value="upcoming">
-                {/* <BookingList 
-                bookingList={filterUserBooking('upcoming')}
-                updateRecord={()=>getUserBookingList()}
-                expired={false}
-                /> */}
             </TabsContent>
             <TabsContent value="expired">
-                {/* <BookingList bookingList={filterUserBooking('expired')}
-                updateRecord={()=>getUserBookingList()}
-                expired={true}
-                /> */}
             </TabsContent>
-            </Tabs>
-
+        </Tabs>
     </div>
   )
 }
